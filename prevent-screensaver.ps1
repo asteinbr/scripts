1f59cc2198a32ce4396a5048e6a06e81c1504546
asteinbr <1075297+asteinbr@users.noreply.github.com>
2022-07-13 17:16:03 +0200

    Create prevent-screensaver.ps1

---
+++ prevent-screensaver.ps1
@@ -0,0 +1,12 @@
+function preventScr() {
+    param($minutes = 60)
+
+    $myshell = New-Object -com "Wscript.Shell"
+
+    for ($i = 0; $i -lt $minutes; $i++) {
+        Start-Sleep -Seconds 60
+        $myshell.sendkeys(".")
+    }
+}
+
+preventSrc
